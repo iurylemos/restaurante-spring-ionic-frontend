ce703232c66a36ea11d43268baa3cbb9b3017104
Iury Lemos <iurylemos10@gmail.com>
2019-06-18 10:10:54 -0300

    Desabilitando menu na tela de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 /*Esse decotador abaixo que vai contar para minha aplicacao 
 que essa classe ela é uma pagina
@@ -21,10 +22,31 @@ com minhas paginas na forma LazyLoadir
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
     //Controlador da view do home.html
+    //O que tiver de argumento no construtor vai ser injetado na classe
 
   }
+  /*
+  ionVieWillEnter() ou seja quando eu entrar na pagina vou desabilitar
+  aquela barralateral que é o menu, pois não quero ela na minha tela de login
+
+  e o ionViewDidLeave() é quando eu sair dessa pagina vou habilita-lo 
+  novamente, para a pagina categorias ter o seu menu lateral.
+  */
+
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+
+
+
+
   //Metodo login sendo criado
   /*abrir a pagina de categorias
     //navegando de uma pagina para outra, utiliza o NavController 
